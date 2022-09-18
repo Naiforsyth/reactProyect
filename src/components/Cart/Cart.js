3d896b54a980b4c41876e2299011472b63e09998
VERGARA Nairet <naiforsyth@gmail.com>
2022-09-18 18:45:11 -0300

    ITEM COLLECTION

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,40 +6,36 @@ import "./Cart.scss";
 const Cart = () => {
   const { cart, cartTotal, emptyCart, removeItem } = useContext(CartContext);
 
-  return (
-
-    cart.length === 0
-      ?
-      <EmptyCart />
-      :
-      <div className="container-cart">
-
-        <h1>Tu Compra</h1>
-        <hr />
-        {cart.map((item) => (
-          <div className="box-cart" key={item.id}>
-            <img className="image-cart" src={item.imagen} alt={item.name} />
-            <div className="content-cart">
-              <h2 className="title-cart">{item.nombre}</h2>
-              <p className="quantity-cart">
-                Cantidad: <span>{item.cantidad}</span>
-              </p>
-              <p className="price-cart">
-                Precio: <span className="number">{item.precio}</span>
-              </p>
-              <button onClick={() => removeItem(item.id)} className="btn-remove">Eliminar del Carrito</button>
-            </div>
+  return cart.length === 0 ? (
+    <EmptyCart />
+  ) : (
+    <div className="container-cart">
+      <h1>Tu Compra</h1>
+      <hr />
+      {cart.map((item) => (
+        <div className="box-cart" key={item.id}>
+          <img className="image-cart" src={item.imagen} alt={item.name} />
+          <div className="content-cart">
+            <h2 className="title-cart">{item.nombre}</h2>
+            <p className="quantity-cart">
+              Cantidad: <span>{item.cantidad}</span>
+            </p>
+            <p className="price-cart">
+              Precio: <span className="number">{item.precio}</span>
+            </p>
+            <button onClick={() => removeItem(item.id)} className="btn-remove">
+              Eliminar del Carrito
+            </button>
           </div>
-        ))}
-        <div className="box-total">
-          <h3 className="total-cart">Total: ${cartTotal()}</h3>
-          <button onClick={emptyCart} className="btn-total">
-            Vaciar Carrito
-          </button>
-          
-
         </div>
+      ))}
+      <div className="box-total">
+        <h3 className="total-cart">Total: ${cartTotal()}</h3>
+        <button onClick={emptyCart} className="btn-total">
+          Vaciar Carrito
+        </button>
       </div>
+    </div>
   );
 };
 
